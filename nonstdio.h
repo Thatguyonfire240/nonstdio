1824979f981e9598e45ab39136b6dce933a6cb70
Thatguyonfire240 <marqsandstrom@gmail.com>
2026-01-01 15:17:28 -0600

    Fixed bugs in codebase to align with standard C practices

--- nonstdio.h
+++ nonstdio.h
@@ -1,26 +1,31 @@
-int a = 'z';
-int b = 'a';
-int c = 'b';
-int d = 'c';
-int e = 'd';
-int f = 'e';
-int g = 'f';
-int h = 'g';
-int i = 'h';
-int j = 'i';
-int k = 'j';
-int l = 'k';
-int m = 'l';
-int n = 'm';
-int o = 'n';
-int p = 'o';
-int q = 'p';
-int r = 'q';
-int s = 'r';
-int t = 's';
-int u = 't';
-int v = 'u';
-int w = 'v';
-int x = 'w';
-int y = 'x';
-int z = 'y';
+#ifndef MYFILE_H
+#define MYFILE_H
+
+extern int a;
+extern int b;
+extern int c;
+extern int d;
+extern int e;
+extern int f;
+extern int g;
+extern int h;
+extern int i;
+extern int j;
+extern int k;
+extern int l;
+extern int m;
+extern int n;
+extern int o;
+extern int p;
+extern int q;
+extern int r;
+extern int s;
+extern int t;
+extern int u;
+extern int v;
+extern int w;
+extern int x;
+extern int y;
+extern int z;
+
+#endif
